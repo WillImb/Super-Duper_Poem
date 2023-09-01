7381219fe9806536abf1accbfc33aa64cd4fb60e
WillImb <143808397+WillImb@users.noreply.github.com>
2023-09-01 09:51:14 -0400

    Project Work Commit

---
+++ MyPoem/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace MyPoem
+{
+    internal class Program
+    {
+         
+
+       static void Main(string[] args)
+        {
+         String name = ("Will");
+         int seconds = 1;
+         float radius = 1f;
+         double width = 4.5;
+         double length = 16.2;
+
+//Write Name then prints Seconds in year 
+         Console.WriteLine(name);
+           
+            seconds = 365 * 24 * 60 * 60;
+            Console.WriteLine(seconds);
+            
+            
+            Console.WriteLine("nothing");
+            
+// Prints perimeter
+
+            Console.WriteLine("Perimeter = " + width*2 + length*2);
+
+
+        for (int i = 0; i < 4;  i++)
+            {
+                Console.WriteLine(i);
+            }
+        
+
+        
+
+        }
+    }
+}
